3618b8d6a2af3737d41cc108362d278d72181919
bidinpithecus <vinibidin@gmail.com>
2024-05-17 00:38:08 -0300

    Helper to plot curves

---
+++ src/plotter.rs
@@ -0,0 +1,47 @@
+use plotly::common::{DashType, Marker, Mode, Title};
+use plotly::{Plot, Scatter};
+
+pub struct Plotter {
+    plot: Plot,
+}
+
+impl Plotter {
+    pub fn new() -> Self {
+        Self { plot: Plot::new() }
+    }
+
+    pub fn line(&mut self, x_values: Vec<f64>, y_values: Vec<f64>, name: &str, is_dashed: bool) {
+        let dash_type: DashType = if is_dashed {
+            plotly::common::DashType::Dash
+        } else {
+            plotly::common::DashType::Solid
+        };
+
+        let trace = Scatter::new(x_values, y_values)
+        .mode(Mode::Lines)
+        .name(name)
+        .line(plotly::common::Line::new().dash(dash_type));
+
+        self.plot.add_trace(trace);
+    }
+
+    pub fn markers(&mut self, x_values: Vec<f64>, y_values: Vec<f64>, name: &str) {
+        let markers = Scatter::new(x_values, y_values)
+        .mode(Mode::Markers)
+        .name(name)
+        .marker(Marker::new().size(10));
+
+        self.plot.add_trace(markers);
+    }
+
+    pub fn plot(&mut self, title: &str, filename: &str, show: bool, save: bool) {
+        self.plot.set_layout(plotly::Layout::new().width(800).height(600).title(Title::from(title)));
+
+        if show {
+            self.plot.show();
+        }
+        if save {
+            self.plot.write_html(filename);
+        }
+    }
+}
